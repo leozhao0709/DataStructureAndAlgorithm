05a95c28bfa6395e8c24cf4749206e385625c692
lzhao <zhao434@usc.edu>
2017-06-12 21:19:20 -0700

    update shell sort, not finish

--- ts/src/sort/shellSort.ts
+++ ts/src/sort/shellSort.ts
@@ -1,25 +1,21 @@
-export const shellSort = <T>(arr: T[]): T[] => {
-  const arrCopy = arr;
-
-
-
-  return arrCopy;
-};
-
-const selectionSort = <T>(arr: T[]): T[] => {
+export const shellSort = <T>(arr: T[] = [], factor: number = 1): T[] => {
   const arrCopy = arr.slice();
 
-  for (let i = 1; i < arr.length; i++) {
-    const currentValue = arrCopy[i];
-    for (let j = i; j > 0; j--) {
-      if (currentValue < arrCopy[j - 1]) {
-        arrCopy[j] = arrCopy[j - 1];
-      } else {
-        arrCopy[j] = currentValue;
-        break;
-      }
-    }
+  if (factor < 1 || factor > arr.length) {
+    throw Error('factor must be greater than 1 and smaller than array length');
   }
+
+  let gap = Math.floor(arrCopy.length / factor);
+
+  while (gap > 0) {
+    for (let i = 0; i < gap; i++) {
+
+    }
+
+    gap = Math.floor(gap / factor);
+  }
+
 
   return arrCopy;
 };
+
