1bbb9129ae14c1ace13ff8c7e37638d17c1207fb
Lei Zhao <zhao434@usc.edu>
2017-08-01 21:38:08 -0700

    2017-08-01 21:38:01 auto check in

---
+++ dataStructure/Java/src/main/java/dp/Lcs.java
@@ -0,0 +1,19 @@
+package dp;
+
+/**
+ * Created by lzhao on 8/1/17.
+ */
+
+class Lcs {
+
+    private String commonString;
+
+    int getLcs(String a, String b) {
+
+        return 0;
+    }
+
+    String getCommonString() {
+        return commonString;
+    }
+}
---
+++ dataStructure/Java/src/test/java/dp/LcsTest.java
@@ -0,0 +1,37 @@
+package dp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lzhao on 8/1/17.
+ */
+public class LcsTest {
+
+    private Lcs lcs;
+
+    @Before
+    public void setUp() throws Exception {
+        this.lcs = new Lcs();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getLcs() throws Exception {
+        // ari or aoi
+        String a = "androidStudio";
+        String b = "algorithm";
+
+        int length = this.lcs.getLcs(a, b);
+        System.out.println("lcs is " + this.lcs.getCommonString() + ", and length is " + length + ";");
+
+    }
+
+}
