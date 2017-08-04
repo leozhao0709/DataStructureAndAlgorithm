2e8a00626bfd152462884d62a36a46c49fbd762e
Lei Zhao <zhao434@usc.edu>
2017-08-04 08:00:05 -0700

    2017-08-04 08:00:03 auto check in

--- algorithm/Java/leetcode/src/main/java/com/lzhao/leetcode/_001_040/_011_ContainerWithMostWater/Solution.java
+++ algorithm/Java/leetcode/src/main/java/com/lzhao/leetcode/_001_040/_011_ContainerWithMostWater/Solution.java
@@ -7,7 +7,31 @@ package com.lzhao.leetcode._001_040._011_ContainerWithMostWater;
 class Solution {
 
     int maxArea(int[] height) {
-        int result = 0;
-        return result;
+        int max = 0;
+
+        // time out
+//        for (int i = 0; i < height.length; i++) {
+//            for (int j = i+1; j < height.length; j++) {
+//                int maxHeight = height[i] > height[j] ? height[j] : height[i];
+//                if (maxHeight * (j - i) > max) {
+//                    max = maxHeight * (j - i);
+//                }
+//            }
+//        }
+
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left ++;
+            } else {
+                right --;
+            }
+        }
+
+
+        return max;
     }
 }
