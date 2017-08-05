56b617f9abe6c2c7c19ffa3d7f69aff5740d3e89
Lei Zhao <zhao434@usc.edu>
2017-08-05 08:00:05 -0700

    2017-08-05 08:00:02 auto check in

---
+++ algorithm/Java/leetcode/src/main/java/com/lzhao/leetcode/_001_040/_015_3Sum/Solution.java
@@ -0,0 +1,17 @@
+package com.lzhao.leetcode._001_040._015_3Sum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lzhao on 8/4/17.
+ */
+
+class Solution {
+
+    List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        return result;
+    }
+}
