9785bf488cfea3f10e35c5313ad8167ddab9b084
lzhao <zhao434@usc.edu>
2017-09-30 10:23:17 -0700

    366

---
+++ algorithm/Java/leetcode/src/main/java/com/lzhao/leetcodeLinkedin/_366_findLeavesOfBT/Solution.java
@@ -0,0 +1,44 @@
+package com.lzhao.leetcodeLinkedin._366_findLeavesOfBT;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by lzhao on 9/29/17.
+ */
+
+class Solution {
+
+    List<List<Integer>> findLeaves(TreeNode root) {
+        int depth = this.depth(root);
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
+
+        for (int i = 0; i < depth; i++)
+        {
+            res.add(new LinkedList<Integer>());
+        }
+
+        this.helper(root, res);
+
+        return res;
+    }
+
+    private void helper(TreeNode root, List<List<Integer>> res) {
+        if (root == null) {
+            return;
+        }
+
+        int depth = this.depth(root);
+        res.get(depth-1).add(root.val);
+        this.helper(root.left, res);
+        this.helper(root.right, res);
+    }
+
+    int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1+Math.max(this.depth(root.left), this.depth(root.right));
+    }
+}
---
+++ algorithm/Java/leetcode/src/main/java/com/lzhao/leetcodeLinkedin/_366_findLeavesOfBT/TreeNode.java
@@ -0,0 +1,12 @@
+package com.lzhao.leetcodeLinkedin._366_findLeavesOfBT;
+
+/**
+ * Created by lzhao on 9/29/17.
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {val = x;}
+}
