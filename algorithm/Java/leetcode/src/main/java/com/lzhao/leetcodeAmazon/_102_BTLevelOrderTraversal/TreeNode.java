3f4e67f7660493616a7e3751a8d5e2c644e63a3d
lzhao <zhao434@usc.edu>
2017-10-07 14:49:08 -0700

    traverse

---
+++ algorithm/Java/leetcode/src/main/java/com/lzhao/leetcodeAmazon/_102_BTLevelOrderTraversal/TreeNode.java
@@ -0,0 +1,13 @@
+package com.lzhao.leetcodeAmazon._102_BTLevelOrderTraversal;
+
+/**
+ * Created by lzhao on 10/6/17.
+ */
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode (int x) {val = x;}
+}
