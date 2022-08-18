c9641eeab36129c5dccb7ff6a164a8883d15ed6a
Lei Zhao <lei.zhao@coupa.com>
2022-08-18 07:00:05 -0700

    add by auto checkIn

---
+++ js_round1/src/leetcode/_151_200/_189_rotate/rotate.ts
@@ -0,0 +1,50 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+// function rotate(nums: number[], k: number): void {
+//   k = k % nums.length;
+//   if (k === 0) {
+//     return;
+//   }
+
+//   const last = nums[nums.length - 1];
+//   let temp;
+//   let prev = nums[0];
+//   for (let i = 0; i + 1 < nums.length; i++) {
+//     temp = nums[i + 1];
+//     nums[i + 1] = prev;
+//     prev = temp;
+//   }
+
+//   nums[0] = last;
+//   rotate(nums, k - 1);
+// }
+
+function rotate(nums: number[], k: number): void {
+  k = k % nums.length;
+
+  if (k === 0) {
+    return;
+  }
+
+  let count = 0;
+  let start = 0;
+  let i = start;
+  let prev = nums[start];
+  while (count < nums.length) {
+    do {
+      const nextIndex = (i + k) % nums.length;
+      const temp = nums[nextIndex];
+      nums[nextIndex] = prev;
+      prev = temp;
+      i = nextIndex;
+      count++;
+    } while (i !== start);
+
+    start++;
+    i = start;
+    prev = nums[start];
+  }
+}
+
+export default rotate;
