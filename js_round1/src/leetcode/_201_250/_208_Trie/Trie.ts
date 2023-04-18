90307b783857f662115f7ffe3138b954ef6f6be2
leozhao0709 <leo.zhao.real@gmail.com>
2023-04-18 15:48:53 +0800

    add by auto checkIn

--- js_round1/src/leetcode/_201_250/_208_Trie/Trie.ts
+++ js_round1/src/leetcode/_201_250/_208_Trie/Trie.ts
@@ -1,58 +1,116 @@
 // https://leetcode.com/problems/implement-trie-prefix-tree/
 
-interface TrieNode {
-  isWord: boolean;
-  next: Map<string, TrieNode>;
+// interface TrieNode {
+//   isWord: boolean;
+//   next: Map<string, TrieNode>;
+// }
+
+// class Trie {
+//   private readonly root: TrieNode;
+
+//   constructor() {
+//     this.root = {
+//       isWord: false,
+//       next: new Map(),
+//     };
+//   }
+
+//   insert(word: string): void {
+//     let curr: TrieNode = this.root;
+//     for (let i = 0; i < word.length; i++) {
+//       const char = word.charAt(i);
+//       if (!curr.next.has(char)) {
+//         curr.next.set(char, { isWord: false, next: new Map() });
+//       }
+//       curr = curr.next.get(char)!;
+//     }
+
+//     if (!curr?.isWord) {
+//       curr.isWord = true;
+//     }
+//   }
+
+//   search(word: string): boolean {
+//     let curr = this.root;
+//     for (let i = 0; i < word.length; i++) {
+//       const char = word.charAt(i);
+//       if (!curr.next.has(char)) {
+//         return false;
+//       }
+//       curr = curr.next.get(char)!;
+//     }
+
+//     return curr.isWord;
+//   }
+
+//   startsWith(prefix: string): boolean {
+//     let curr = this.root;
+//     for (let i = 0; i < prefix.length; i++) {
+//       const char = prefix.charAt(i);
+//       if (!curr.next.has(char)) {
+//         return false;
+//       }
+//       curr = curr.next.get(char)!;
+//     }
+
+//     return true;
+//   }
+// }
+
+// interface TrieNode {
+//   isWord: boolean;
+//   next?: Map<string, TrieNode>;
+// }
+
+class TrieNode {
+  constructor(
+    public currWord: string | undefined = undefined,
+    public isWord = false,
+    public next: Map<string, TrieNode> | undefined = undefined
+  ) {}
 }
 
 class Trie {
-  private readonly root: TrieNode;
+  private root: TrieNode;
 
   constructor() {
-    this.root = {
-      isWord: false,
-      next: new Map(),
-    };
+    this.root = new TrieNode();
   }
 
   insert(word: string): void {
-    let curr: TrieNode = this.root;
-    for (let i = 0; i < word.length; i++) {
-      const char = word.charAt(i);
+    let curr = this.root;
+    for (const char of word) {
+      if (curr.next === undefined) {
+        curr.next = new Map();
+      }
       if (!curr.next.has(char)) {
-        curr.next.set(char, { isWord: false, next: new Map() });
+        curr.next.set(char, new TrieNode(char));
       }
       curr = curr.next.get(char)!;
     }
 
-    if (!curr?.isWord) {
-      curr.isWord = true;
-    }
+    curr.isWord = true;
   }
 
   search(word: string): boolean {
     let curr = this.root;
-    for (let i = 0; i < word.length; i++) {
-      const char = word.charAt(i);
-      if (!curr.next.has(char)) {
+    for (const char of word) {
+      if (curr.next === undefined || !curr.next.has(char)) {
         return false;
       }
       curr = curr.next.get(char)!;
     }
-
     return curr.isWord;
   }
 
   startsWith(prefix: string): boolean {
     let curr = this.root;
-    for (let i = 0; i < prefix.length; i++) {
-      const char = prefix.charAt(i);
-      if (!curr.next.has(char)) {
+    for (const char of prefix) {
+      if (curr.next === undefined || !curr.next.has(char)) {
         return false;
       }
       curr = curr.next.get(char)!;
     }
-
     return true;
   }
 }
